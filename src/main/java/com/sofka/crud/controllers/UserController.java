c2af99ef4b775ac65a66bfb88318ca551fedd911
Juan Vargas <juandeveloper19@gmail.com>
2022-04-28 12:29:58 -0500

    Methods to get all users

--- src/main/java/com/sofka/crud/controllers/UserController.java
+++ src/main/java/com/sofka/crud/controllers/UserController.java
@@ -1,6 +1,25 @@
 package com.sofka.crud.controllers;
 
-public class UserController {
+import java.util.ArrayList;
 
+import com.sofka.crud.models.UserModel;
+import com.sofka.crud.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    // Method to get all users
+    @GetMapping()
+    public ArrayList<UserModel> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+  
 }
 
--- src/main/java/com/sofka/crud/services/UserService.java
+++ src/main/java/com/sofka/crud/services/UserService.java
@@ -1,8 +1,22 @@
 package com.sofka.crud.services;
 
+import java.util.ArrayList;
 
+import com.sofka.crud.models.UserModel;
+import com.sofka.crud.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
-    
+    @Autowired
+    UserRepository userRepository;
+
+    // Method to get all users
+    public ArrayList<UserModel> getAllUsers() {
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
 
 }
 
